d035af9a5146e47f08b3ac07909332f67e324c3b
leeyanggoo <didrn94553@gmail.com>
2024-04-27 17:34:01 +0900

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+// import { Button } from '@mui/material';
 import './App.css';
+// import Type from './Type';
+
+// const Button = ({ text }) => {
+//   return (
+//     <button
+//       style={{
+//         padding: '10px',
+//         backgroundColor: 'blue',
+//         color: 'white',
+//         borderRadius: '5px',
+//         border: 'none',
+//         cursor: 'pointer',
+//       }}
+//     >
+//       {text}
+//     </button>
+//   );
+// };
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>Hello World</h1>
+      <button className='p-5 rounded-lg bg-blue-500'>Click Me</button>
+      dksehl?
     </div>
   );
 }
---
+++ src/TextBox.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const TextBox = () => {
+  return <div>TextBox</div>;
+};
+
+export default TextBox;
